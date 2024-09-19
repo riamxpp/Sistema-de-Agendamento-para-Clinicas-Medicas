5b37af92aa7799c6e6415273501921ef73f034b5
jGean <josegeantlc@gmail.com>
2024-09-18 23:27:24 -0300

    Ultimas Telas

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ void tela_sobre(void);
 void tela_paciente(void);
 void tela_agendamento(void);
 void tela_informacoes(void);
+void tela_medico(void);
+void tela_procedimentos(void);
+void tela_sobre_projeto(void);
 
 int main() {
   tela_menu_principal();
@@ -13,6 +16,9 @@ int main() {
   tela_paciente();
   tela_agendamento();
   tela_informacoes();
+  tela_medico();
+  tela_procedimentos();
+  tela_sobre_projeto();
   return 0;
 }
 
@@ -24,7 +30,7 @@ void tela_sobre(void) {
   printf("***                 Centro de Ensino Superior do Seridó                     ***\n");
   printf("***                    Curso Sistemas de informações                        ***\n");
   printf("***                  Disciplina DCT1106 -- Programação                      ***\n");
-  printf("***                  Projeto Sistema de Gestão Escolar                      ***\n");
+  printf("***             Sistema de Agendamento para Clinicas Medicas                ***\n");
   printf("***             Developed by @riamxpp and @jGean09 - 09/2024                ***\n");
   printf("***                                                                         ***\n");
   printf("*******************************************************************************\n");
@@ -99,7 +105,7 @@ void tela_informacoes(void) {
   printf("***           ------ INFORMAÇÕES SOBRE OS DESENVOLVEDORES -----             ***\n");
   printf("***       Riam Stefesom                           Gean Macedo               ***\n");
   printf("***       23 anos                                 22 anos                   ***\n");
-  printf("***       Estudante                               EStudante                 ***\n");
+  printf("***       Estudante                               Estudante                 ***\n");
   printf("***       Caicó - RN                              Caicó - RN                ***\n");
   printf("***       github.com/riamxpp                      github.com/jGean09        ***\n");
   printf("***                                                                         ***\n");
@@ -108,3 +114,69 @@ void tela_informacoes(void) {
   printf("\n");
   printf("Pressione a tecla <ENTER> para continuar...\n");
 }
+
+void tela_medico(void) {
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***                 ------ MÉDICOS ------                                   ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 1. Cadastrar Médico                                     ***\n");
+  printf("***                 2. Pesquisar Médico                                     ***\n");
+  printf("***                 3. Atualizar Médico                                     ***\n");
+  printf("***                 4. Remover Médico                                       ***\n");
+  printf("***                 0. Cancelar e sair                                      ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 Escolha a opção desejada:                               ***\n");
+  printf("***                                                                         ***\n");
+  printf("*******************************************************************************\n");
+  printf("\n");
+  printf("Pressione a tecla <ENTER> para continuar...\n");
+}
+
+void tela_procedimentos(void) {
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***                 ------ PROCEDIMENTOS ------                             ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 1. Cadastrar Procedimento                               ***\n");
+  printf("***                 2. Pesquisar Procedimento                               ***\n");
+  printf("***                 3. Atualizar Procedimento                               ***\n");
+  printf("***                 4. Remover Procedimento                                 ***\n");
+  printf("***                 0. Cancelar e sair                                      ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 Escolha a opção desejada:                               ***\n");
+  printf("***                                                                         ***\n");
+  printf("*******************************************************************************\n");
+  printf("\n");
+  printf("Pressione a tecla <ENTER> para continuar...\n");
+}
+
+void tela_sobre_projeto(void){
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***                               SIG-Clinic                                ***\n");
+  printf("***             Sistema de Agendamento para Clinicas Medicas                ***\n");
+  printf("***                  Disciplina DCT1106 -- Programação                      ***\n");
+  printf("***             Developed by @riamxpp and @jGean09 - 09/2024                ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                              Objetivo:                                  ***\n");
+  printf("***       Este sistema tem como objetivo facilitar o agendamento de         ***\n");
+  printf("***    consultas em clinicas medicas, permitindo que pacientes marquem      ***\n");
+  printf("***    suas consultas de forma mais eficiente e que as clinicas gerenciem   ***\n");
+  printf("***    seus horarios de atendimento.                                        ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                          Tecnologia Utilizadas:                         ***\n");
+  printf("***                      - Linguagem de Programacao C                       ***\n");
+  printf("***                                                                         ***\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("*******************************************************************************\n");
+  printf("\n");
+  printf("Pressione a tecla <ENTER> para continuar...\n");
+}
+
+
