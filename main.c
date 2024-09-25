b84d3e6493d0a54d51389548390aa2333d0cd60a
Riam Stefesom <riamstefeson616@gmail.com>
2024-09-25 20:55:35 -0300

    refactor: transferindo função 'tela_agendamento' para o arquivo 'agendamento.c'

--- main.c
+++ main.c
@@ -61,25 +61,6 @@ void tela_menu_principal(void) {
   printf("Pressione a tecla <ENTER> para continuar...\n");
 }
 
-void tela_agendamento(void) {
-  printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***                 ------ AGENDAMENTO ------                               ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 1. Cadastrar Agendamento                                ***\n");
-  printf("***                 2. Pesquisar Agendamento                                ***\n");
-  printf("***                 3. Atualizar Agendamento                                ***\n");
-  printf("***                 4. Remover Agendamento                                  ***\n");
-  printf("***                 0. Cancelar e sair                                      ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 Escolha a opção desejada:                               ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
-  printf("\n");
-  printf("Pressione a tecla <ENTER> para continuar...\n");
-}
-
 void tela_informacoes(void) {
   printf("\n");
   printf("*******************************************************************************\n");
