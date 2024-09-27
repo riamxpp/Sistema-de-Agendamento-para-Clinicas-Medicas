7b945f718d076572a542a441fc534c8569bd8f68
jGean <josegeantlc@gmail.com>
2024-09-27 15:44:01 -0300

    refactor: removendoa função  'tela_procedimentos' do arquivo 'main.c'

--- main.c
+++ main.c
@@ -81,27 +81,6 @@ void tela_informacoes(void) {
   printf("Pressione a tecla <ENTER> para continuar...\n");
 }
 
-
-
-void tela_procedimentos(void) {
-  printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***                 ------ PROCEDIMENTOS ------                             ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 1. Cadastrar Procedimento                               ***\n");
-  printf("***                 2. Pesquisar Procedimento                               ***\n");
-  printf("***                 3. Atualizar Procedimento                               ***\n");
-  printf("***                 4. Remover Procedimento                                 ***\n");
-  printf("***                 0. Cancelar e sair                                      ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 Escolha a opção desejada:                               ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
-  printf("\n");
-  printf("Pressione a tecla <ENTER> para continuar...\n");
-}
-
 void tela_sobre_projeto(void){
   printf("\n");
   printf("*******************************************************************************\n");
