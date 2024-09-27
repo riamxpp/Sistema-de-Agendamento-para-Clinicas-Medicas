f1ae878def6a4f1ce143d1c7cdea3205cf18e31b
jGean <josegeantlc@gmail.com>
2024-09-27 15:39:15 -0300

    refactor: transferindo a função 'tela_procedimentos' para o arquivo 'procedimento.c'

---
+++ procedimento.c
@@ -0,0 +1,19 @@
+
+void tela_procedimentos(void) {
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***                 ------ PROCEDIMENTOS ------                             ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 1. Cadastrar Procedimento                               ***\n");
+  printf("***                 2. Pesquisar Procedimento                               ***\n");
+  printf("***                 3. Atualizar Procedimento                               ***\n");
+  printf("***                 4. Remover Procedimento                                 ***\n");
+  printf("***                 0. Cancelar e sair                                      ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 Escolha a opção desejada:                               ***\n");
+  printf("***                                                                         ***\n");
+  printf("*******************************************************************************\n");
+  printf("\n");
+  printf("Pressione a tecla <ENTER> para continuar...\n");
+}
