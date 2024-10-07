a828e54fc58f6338ca88bac6bf6ddd0c3ca518b9
Riam Stefesom <riamstefeson616@gmail.com>
2024-10-07 17:48:34 -0300

    feat: criando switch case para o menu principal.

--- main.c
+++ main.c
@@ -32,34 +32,51 @@ void tela_deletar_procedimento();
 void tela_ver_procedimento();
 
 int main() {
-  tela_sobre();
-  tela_menu_principal();
-  tela_informacoes();
-  tela_sobre_projeto();
-
-  tela_paciente();
   tela_cadastrar_paciente();
   tela_atualizar_paciente();
   tela_deletar_paciente();
   tela_ver_paciente();
 
-  tela_agendamento();
   tela_cadastrar_agendamento();
   tela_atualizar_agendamento();
   tela_deletar_agendamento();
   tela_ver_agendamento();
   
-  tela_medico();
   tela_cadastrar_medico();
   tela_atualizar_medico();
   tela_deletar_medico();
   tela_ver_medico();
   
-  tela_procedimentos();
+  
   tela_cadastrar_procedimento();
   tela_atualizar_procedimento();
   tela_deletar_procedimento();
   tela_ver_procedimento();
+
+  tela_menu_principal();
+  int opcao;
+  
+  scanf("%d", &opcao);
+  switch (opcao) {
+    case 1:
+      tela_paciente();
+      break;
+    case 2:
+      tela_medico();
+      break;
+    case 3:
+      tela_procedimentos();
+      break;
+    case 4:
+      tela_agendamento();
+      break;
+    case 5:
+      tela_informacoes();
+      break;
+    case 6:
+      tela_sobre();
+      break;
+  }
   return 0;
 }
 
@@ -74,52 +91,6 @@ void tela_sobre(void) {
   printf("***             Sistema de Agendamento para Clinicas Medicas                ***\n");
   printf("***             Developed by @riamxpp and @jGean09 - 09/2024                ***\n");
   printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
-  printf("\n");
-  printf("Pressione a tecla <ENTER> para continuar...\n");
-}
-
-void tela_menu_principal(void) { 
-  printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***            Sistema de Agendamento para Clinicas Medicas                 ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 1. Módulo Paciente                                      ***\n");
-  printf("***                 2. Módulo Médico                                        ***\n");
-  printf("***                 3. Módulo Procedimento                                  ***\n");
-  printf("***                 4. Módulo Agendamento                                   ***\n");
-  printf("***                 5. Módulo Relatórios                                    ***\n");
-  printf("***                 5. Módulo Informações                                   ***\n");
-  printf("***                 5. Módulo Sobre                                         ***\n");
-  printf("***                 0. Sair                                                 ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                 Escolha a opção desejada:                               ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
-  printf("\n");
-  printf("Pressione a tecla <ENTER> para continuar...\n");
-}
-
-void tela_informacoes(void) {
-  printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***           ------ INFORMAÇÕES SOBRE OS DESENVOLVEDORES -----             ***\n");
-  printf("***       Riam Stefesom                           Gean Macedo               ***\n");
-  printf("***       23 anos                                 22 anos                   ***\n");
-  printf("***       Estudante                               Estudante                 ***\n");
-  printf("***       Caicó - RN                              Caicó - RN                ***\n");
-  printf("***       github.com/riamxpp                      github.com/jGean09        ***\n");
-  printf("***                                                                         ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
-  printf("\n");
-  printf("Pressione a tecla <ENTER> para continuar...\n");
-}
-
-void tela_sobre_projeto(void){
-  printf("\n");
   printf("*******************************************************************************\n");
   printf("***                                                                         ***\n");
   printf("***                               SIG-Clinic                                ***\n");
@@ -138,6 +109,43 @@ void tela_sobre_projeto(void){
   printf("***                      - Linguagem de Programacao C                       ***\n");
   printf("***                                                                         ***\n");
   printf("*******************************************************************************\n");
+  printf("\n");
+  printf("Pressione a tecla <ENTER> para continuar...\n");
+}
+
+void tela_menu_principal(void) { 
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***            Sistema de Agendamento para Clinicas Medicas                 ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 1. Módulo Paciente                                      ***\n");
+  printf("***                 2. Módulo Médico                                        ***\n");
+  printf("***                 3. Módulo Procedimento                                  ***\n");
+  printf("***                 4. Módulo Agendamento                                   ***\n");
+  printf("***                 5. Módulo Relatórios                                    ***\n");
+  printf("***                 6. Módulo Informações                                   ***\n");
+  printf("***                 7. Módulo Sobre                                         ***\n");
+  printf("***                 0. Sair                                                 ***\n");
+  printf("***                                                                         ***\n");
+  printf("***                 Escolha a opção desejada: ");
+  // printf("***                                                                         ***\n");
+  // printf("*******************************************************************************\n");
+  // printf("\n");
+  // printf("Pressione a tecla <ENTER> para continuar...\n");
+}
+
+void tela_informacoes(void) {
+  printf("\n");
+  printf("*******************************************************************************\n");
+  printf("***                                                                         ***\n");
+  printf("***           ------ INFORMAÇÕES SOBRE OS DESENVOLVEDORES -----             ***\n");
+  printf("***       Riam Stefesom                           Gean Macedo               ***\n");
+  printf("***       23 anos                                 22 anos                   ***\n");
+  printf("***       Estudante                               Estudante                 ***\n");
+  printf("***       Caicó - RN                              Caicó - RN                ***\n");
+  printf("***       github.com/riamxpp                      github.com/jGean09        ***\n");
+  printf("***                                                                         ***\n");
   printf("***                                                                         ***\n");
   printf("*******************************************************************************\n");
   printf("\n");
