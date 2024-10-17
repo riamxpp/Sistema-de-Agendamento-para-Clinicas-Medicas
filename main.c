cf03c287f0655ee464f9fbe7da383c61f31166c7
jGean <josegeantlc@gmail.com>
2024-10-17 00:08:38 -0300

    refactor: Movendo assinaturas de paciente do 'mainc .c' para o 'paciente.h'

--- main.c
+++ main.c
@@ -20,12 +20,7 @@ void tela_cadastrar_medico();
 void tela_atualizar_medico();
 void tela_deletar_medico();
 void tela_ver_medico();
-// PACIENTE
-void tela_paciente();
-void tela_cadastrar_paciente();
-void tela_atualizar_paciente();
-void tela_deletar_paciente();
-void tela_ver_paciente();
+
 // AGENDAMENTO
 void tela_agendamento(void);
 void tela_cadastrar_agendamento();
---
+++ paciente.h
@@ -0,0 +1,6 @@
+// ASSINATURA DE PACIENTE
+void tela_paciente();
+void tela_cadastrar_paciente();
+void tela_atualizar_paciente();
+void tela_deletar_paciente();
+void tela_ver_paciente();
