5c164cc7e67cf97c9ce8274dbeebae66328755bc
jGean <josegeantlc@gmail.com>
2024-10-18 17:36:27 -0300

    refactor: movendo todas as funções de relatorio do 'main.c' para o arquivo 'relatorio.c' e para 'relatorio.h'

---
+++ relatorio.h
@@ -0,0 +1,7 @@
+
+// Assinaturas RELATORIO
+void tela_relatorio(void);
+void tela_relatorio_medico(void);
+void tela_relatorio_paciente(void);
+void tela_relatorio_procedimento(void);
+void tela_relatorio_agendamento(void);
